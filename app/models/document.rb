ce1909accb2a6e3be1579e45ad9e091bfc00350c
Igor <pires.igor2@gmail.com>
2024-08-13 20:35:37 -0300

    feat(document): Set default status to pending for new documents

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,21 +3,30 @@ class Document < ApplicationRecord
   belongs_to :user
 
   # Enums
-  enum status: { pedding: 0, processing: 1, processed: 2, failed: 3 }
+  enum status: { pending: 0, processing: 1, processed: 2, failed: 3 }
 
   # ActiveStorage
   has_one_attached :xml_file
 
   # Validations
   validates :xml_file, presence: true
+  validate :correct_xml_mime_type
 
   # Callbacks
-  after_create :set_file_name
+  before_create :set_file_name, :default_status
 
   private
 
   def set_file_name
-    xml_file.filename.to_s if xml_file.attached?
+    self.file_name = xml_file.filename.to_s if xml_file.attached?
+  end
+
+  def default_status
+    self.status = :pending
+  end
+
+  def correct_xml_mime_type
+    errors.add(:xml_file, 'deve ser um arquivo XML') unless xml_file.content_type.in?(%w(application/xml text/xml))
   end
 
 end
