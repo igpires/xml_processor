65a05e3d557bb4d16271e4acf6cc3bbe5a9740ad
Igor <pires.igor2@gmail.com>
2024-08-13 20:37:26 -0300

    feat(documents): add routes for index, new and create

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
 
+  resources :documents, only: [:index, :new, :create]
+
   root to: 'home#index'
 end
